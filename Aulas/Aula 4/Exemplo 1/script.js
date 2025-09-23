f60db01ea33d424e87c2c09241a3399aea836002
FelipeFlores77 <ruansilvaad012@gmail.com>
2025-09-23 19:41:53 -0300

    Adicionado a pasta aulas

---
+++ Aulas/Aula 4/Exemplo 1/script.js
@@ -0,0 +1,13 @@
+// Exemplo 1 
+// Felipe Flores 
+
+console.log("Amanha é terça")
+console.log("vai vasco")
+
+prompt("Qual seu nome?")
+
+
+const nome = "Felipe Flores"
+const idade = 16
+
+console.log("O nome é:", nome, "e sua idade é:", idade)
---
+++ Aulas/Exercicios/scripit.js
@@ -0,0 +1,7 @@
+const nome = "Felipe"
+const sobrenome = "Flores"
+const idade = "16"
+const souEstudante = true
+
+console.log("Meu nome é", nome, "meu sobrenome é",sobrenome, "e minha idade é", idade, "e eu sou estudante", souEstudante)
+
