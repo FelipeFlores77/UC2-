0a1eaea46ef68316537dcdd9c43149a5f08dcdcc
FelipeFlores77 <ruansilvaad012@gmail.com>
2025-09-23 21:49:36 -0300

    Alterado os arquivos da aula 5

--- Aulas/Aula 4/Exercicios/Exemplos1/script.js
+++ Aulas/Aula 4/Exercicios/Exemplos1/script.js
@@ -5,9 +5,3 @@ console.log("Amanha é terça")
 console.log("vai vasco")
 
 prompt("Qual seu nome?")
-
-
-const nome = "Felipe Flores"
-const idade = 16
-
-console.log("O nome é:", nome, "e sua idade é:", idade)
