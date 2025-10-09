873b0cac891cfd50413950fb36c5b0c96632c86c
FelipeFlores77 <ruansilvaad012@gmail.com>
2025-10-08 21:53:28 -0300

    Adicionado os arquivos da aula 9

--- Aulas/Aula9/Exercicios/Exercicio1.js
+++ Aulas/Aula9/Exercicios/Exercicio1.js
@@ -72,7 +72,7 @@ console.log(meusPeixes)
 meusPeixes.pop(meusPeixes)
 console.log(meusPeixes)
 
-*/
+
 const letras = [ "A","B","C","D","E","F","G","H"]
 letras.splice(1,7)
 console.log(letras)
@@ -83,7 +83,70 @@ numeros.push("7")
 numeros.splice(3,2)
 console.log("O tamanho do array é de",numeros.length,"numeros.")
 console.log("Os numeros que estao no array atualmente são =",numeros)
+*/
 
-a
-b
-c.10
+let array
+console.log('a. ',array)
+// a. undefined,pois nao fui atribuido nada a array. 
+array = null
+console.log('b. ',array)
+// b. null,pois foi atribuido nulo a array.
+array = [3,4,5,6,7,8,9,10,11,12,13]
+console.log('c. ',array.length)
+// c. 11,ira imprimir o tamanho da array,pois foi usado do comando (length) 
+let i = 0
+console.log('d. ',array[i])
+// d. 3,aqui ira imprimir a array e o numero corespondente ao indice "i" que é 3.
+array[i+1] = 19
+console.log('e ',array)
+// e. 3,19,5,6,7,8,9,10,11,12,13,ira imprimir a nova array substituindo 4 por 19.
+const valor = array[i+6]
+console.log('f. ',valor)
+// f. 9,vai imprimir 9 pois o "i" é o indice 0(quem esta no indice 0 é o numero 3),pulando 6 casas chegara no 9 cujo indice é 6.
+
+
+/*const frase = prompt("Digite uma frase")
+console.log(frase.toUpperCase().replace.All("A","I"), frase.length)
+//SUBI NUM ÔNIBUS EM MIRROCOS,27
+
+
+let nome = prompt("Qual seu nome?")
+let email = prompt("Qual seu email?")
+
+console.log("O email",` ${email} `,"foi cadastrado com sucesso.Seja bem vinda(o)",` ${nome} `,".")
+*/
+/*
+let comidasFavoritas = ["Lasanha","Churrasco","Panqueca","Strogonoff","Pizza"]
+console.log(comidasFavoritas)
+console.log("Essas são minhas comidas preferidas")
+console.log(comidasFavoritas[0])
+console.log(comidasFavoritas[1])
+console.log(comidasFavoritas[2])
+console.log(comidasFavoritas[3])
+console.log(comidasFavoritas[4])
+
+let comidasFavoritaUsuario = prompt("Me fale sua comida favorita.")
+comidasFavoritas = ["Lasanha",comidasFavoritaUsuario,"Panqueca","Strogonoff","Pizza"]
+console.log(comidasFavoritas)
+*/
+/*
+let atividade1 = prompt("Me fale uma atividade que voce precisa realizar no seu dia.")
+let atividade2 = prompt("Me fale outra atividade que voce precisa realizar no seu dia")
+let atividade3 = prompt("Me fale outra atividade que voce precisa realizar no seu dia")
+let listaDeTarefas = [atividade1,atividade2,atividade3]
+console.log(listaDeTarefas)
+let indice = prompt("Digite o indice da tarefa que voce ja realizou hoje.")
+listaDeTarefas.pop(indice)
+console.log(listaDeTarefas)
+*/
+
+/*let frase = prompt("Me fale uma frase.")
+let fraseEmPalvras = frase.split(" ")
+console.log(fraseEmPalvras)
+*/
+
+
+let frutas = ["Bananas","Morango","Abacaxi","Laranaja","Ameixa"]
+let indice = frutas.indexOf("Abacaxi")
+console.log("O indice do Abacaxi é",indice,".")
+console.log("O tamanho do array é",frutas.length,".")
