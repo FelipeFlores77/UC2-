34efb30df2b5d4ff05770c0d4528b25930b03650
FelipeFlores77 <ruansilvaad012@gmail.com>
2025-11-04 21:50:52 -0300

    Adicionado os arquivos da aula 15

---
+++ Aulas/Aula14/Funções.js
@@ -0,0 +1,75 @@
+/*function imprimirOlaMundo(){
+    console.log("Olá mundo!!")
+}
+
+
+imprimirOlaMundo();
+
+function imprimirBemVindo(){
+    console.log("Bem vindos turma 25-2N!")
+}
+
+imprimirBemVindo();
+
+function calcularNumeros(){
+    let resultado = 7 + 4
+    console.log(`A soma é : ${resultado}`)
+}
+
+calcularNumeros();
+
+function dobrarNumero(){
+    let dobrar = 7 * 2
+    console.log(`O dobro de 7 é : ${dobrar}`)
+}
+
+dobrarNumero();
+
+function nomes(nome){
+    console.log(`Olá ${nome}`)
+}
+    nomes("Felipe")
+    nomes("Gabriel")
+    nomes("Henrique")
+
+    function soma(numero1,numero2){
+        let resulatdo = numero1 + numero2
+        console.log(resulatdo)
+    }
+
+    soma(7,7);
+
+
+    function imprimirVariavel(){
+const b = 2
+console.log("A variavel a:",a)
+console.log("A variavel b",b)
+
+    }
+    imprimirVariavel()
+
+    console.log("A variavel")
+
+
+
+
+    function calculaArea (altura,largura){
+        let area = altura * largura
+        return area
+    }
+    let resultadoArea = calculaArea(5,4)
+    console.log(resultadoArea)
+    console.log(calculaArea(5,4))
+
+    */
+   function numeros(num1,num2){
+    let soma = num1 + num2
+    return soma
+   }
+   let reusltado = numeros(6,4)
+   console.log(reusltado)
+
+function [1,2,3,4,5,6]{
+    let novoArray
+    return novoArray
+}
